7f7a53611e156832afdb1f4550b446ed8fd7e160
Jacques Lucke <mail@jlucke.com>
2024-02-21 11:41:03 +0100

    initial code

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const checked_in = {};
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.post("/check_in", (req, res) => {
+  checked_in[req.body.uuid] = req.body.name;
+  console.log(checked_in);
+  res.send("ok");
+});
+
+app.post("/check_out", (req, res) => {
+  delete checked_in[req.body.uuid];
+  console.log(checked_in);
+  res.send("ok");
+});
+
+app.get("/checked_in", (req, res) => {
+  res.json(checked_in);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
